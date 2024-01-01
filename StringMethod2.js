4c70fb4786c492695deeb811e9ba983c6792fd70
Raja V <rajamails19@gmail.com>
2023-12-31 20:28:06 -0500

    Create StringMethod2.js

---
+++ StringMethod2.js
@@ -0,0 +1,18 @@
+
+// String Methods:
+
+const text = 'JavaScript is fun!';
+console.log(text.toUpperCase());
+console.log(text.toLowerCase());
+console.log(text.indexOf('is')); // Returns the index of the first occurrence
+console.log(text.includes('fun')); // Returns true if the string contains 'fun'
+
+
+/*
+Output:
+JAVASCRIPT IS FUN!
+javascript is fun!
+11
+true
+*/
+
