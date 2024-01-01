bd76543dfb76c634e926e62bb6565aaf3d4b15f1
Raja V <rajamails19@gmail.com>
2023-12-31 20:26:59 -0500

    Create Slice.js

---
+++ Slice.js
@@ -0,0 +1,15 @@
+
+// Substring and Slice:
+
+const sentence = 'This is a sample sentence.';
+const subString = sentence.substring(5, 10);
+const slicedString = sentence.slice(5, 10);
+console.log(subString); // 'is a '
+console.log(slicedString); // 'is a '
+
+
+/*
+Output:
+is a 
+is a 
+*/
