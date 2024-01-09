55283dd44beb37cdad1a2c75269cf526dafc334b
Raja V <rajamails19@gmail.com>
2024-01-08 22:41:24 -0500

    Update Slice.js

--- Slice.js
+++ Slice.js
@@ -2,9 +2,13 @@
 // Substring and Slice:
 
 const sentence = 'This is a sample sentence.';
+
 const subString = sentence.substring(5, 10);
+
 const slicedString = sentence.slice(5, 10);
+
 console.log(subString); // 'is a '
+
 console.log(slicedString); // 'is a '
 
 
